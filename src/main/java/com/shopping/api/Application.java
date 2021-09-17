1ec32b12c1bbbfef8c43abb0fa4b530e2e84561f
azxcv24 <azxcv24@users.noreply.github.com>
2021-09-18 00:42:04 +0900

    0.0.1

---
+++ src/main/java/com/shopping/api/Application.java
@@ -0,0 +1,12 @@
+package com.shopping.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
+//스프링부트를 이용하여 톰켓서버을 가동시작
